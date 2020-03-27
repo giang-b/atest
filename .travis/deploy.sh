10be42bf1a20a370643bebb35ab827245bebbc1e
tui la giang <giang.buiquang@gmail.com>
2020-03-27 14:26:41 +0700

    deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,6 @@ set -e
 chmod 400 .travis/deploy_rsa
 ssh -i .travis/deploy_rsa -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 ssh -i .travis/deploy_rsa -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST docker pull giangbuiquang/aatest
+ssh -i .travis/deploy_rsa -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST docker run --name aatest --rm -d -p 80:80/tcp giangbuiquang/aatest
 
 
