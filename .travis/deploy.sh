f5aad50c1282ec6d6a6f28cd230515759b2ff2d1
tui la giang <giang.buiquang@gmail.com>
2020-03-27 13:52:05 +0700

    deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,3 +3,5 @@ ssh -i .travis/deploy_rsa -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker pull giangbuiquang/aatest
 
+# 34.71.77.172 
+
