4d9b6bb3a6d6a92085cac2597c1801650374a488
tui la giang <giang.buiquang@gmail.com>
2020-03-27 15:52:43 +0700

    deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,9 @@ set -e
 chmod 400 .travis/deploy_rsa
 ssh -i .travis/deploy_rsa $REMOTE_USER@$REMOTE_HOST << EOF
   sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-  sudo docker pull giangbuiquang/aatest:development
+  sudo docker pull giangbuiquang/aatest:dev-$DEV_VERSION
   sudo docker stop aatest || true
-  sudo docker run --name aatest --rm -d -p 80:80/tcp giangbuiquang/aatest:development
+  sudo docker run --name aatest --rm -d -p 80:80/tcp giangbuiquang/aatest:dev-$DEV_VERSION
 EOF
 
 
