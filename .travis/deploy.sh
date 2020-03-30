35414588a7312cb6fc9259c4facbc152d6170512
tui la giang <giang.buiquang@gmail.com>
2020-03-30 17:03:22 +0700

    deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,8 @@
 set -e
 
 chmod 400 .travis/deploy_rsa
+echo $REMOTE_SERVER_USER
+echo $REMOTE_SERVER
 ssh -o StrictHostKeyChecking=no -i .travis/deploy_rsa $REMOTE_SERVER_USER@$REMOTE_SERVER << EOF
   sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
   sudo docker pull $DOCKER_USERNAME/$PROJECT_NAME:$VERSION
