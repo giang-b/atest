9a50634ffe1001561e6a5bae255ffdd659d1ab96
tui la giang <giang.buiquang@gmail.com>
2020-03-30 17:13:08 +0700

    deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,12 +3,12 @@ set -e
 chmod 400 .travis/deploy_rsa
 echo $REMOTE_SERVER_USER
 echo $REMOTE_SERVER
-ssh -o StrictHostKeyChecking=no -i .travis/deploy_rsa $REMOTE_SERVER_USER@$REMOTE_SERVER << EOF
-  sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-  sudo docker pull $DOCKER_USERNAME/$PROJECT_NAME:$VERSION
-  sudo docker stop $PROJECT_NAME || true
-  sudo docker run --name $PROJECT_NAME --rm -d -p 80:80/tcp $DOCKER_USERNAME/$PROJECT_NAME:$VERSION
-EOF
+# ssh -o StrictHostKeyChecking=no -i .travis/deploy_rsa $REMOTE_SERVER_USER@$REMOTE_SERVER << EOF
+#   sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+#   sudo docker pull $DOCKER_USERNAME/$PROJECT_NAME:$VERSION
+#   sudo docker stop $PROJECT_NAME || true
+#   sudo docker run --name $PROJECT_NAME --rm -d -p 80:80/tcp $DOCKER_USERNAME/$PROJECT_NAME:$VERSION
+# EOF
 
 
 
